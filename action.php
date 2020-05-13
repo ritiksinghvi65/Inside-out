f8ad7a5c1a30d88cd8bfb19b281992a69e307cfb
Ritik <ritiksinghvi65@gmail.com>
2020-05-13 10:26:52 +0530

    new changes

--- action.php
+++ action.php
@@ -1,15 +1,22 @@
 <?php
 
-$name = $_REQUEST["name"];
-$phone = $_REQUEST["phone"];
-$email = $_REQUEST["email"];
+$name = $_POST["name"];
+$phone = $_POST["phone"];
+$email = $_POST["email"];
+
+$to = "mail.theinsidestory@gmail.com";
+$subject = "Detail";
+$message = "Name:".$name."\n"."Phone:" .$phone;
+$headers = "From:".$email;
+
+
 
 if(empty($name) || empty($phone) || empty($email))
 {
   echo "Please fill all the details"
 }
 else {
- mail("mail.theinsidestory@gmail.com", "Detail", $phone, "From: $name <$email>");
+ mail($to, $subject , $message , $headers );
  echo "<script type='text/javascript'>alert(Your Appointment Booked Successfully!);
 window.hisory.log(-1);
  </script>";
